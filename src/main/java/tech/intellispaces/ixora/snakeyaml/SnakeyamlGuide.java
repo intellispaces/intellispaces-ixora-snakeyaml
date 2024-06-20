d2836d76114961e12a9557f746985d2df2cf2b44
lavor <lavor.andrey@gmail.com>
2024-06-20 10:37:16 +0600

    SnakeyamlGuide and SnakeyamlGuideTest updated

--- src/main/java/tech/intellispaces/ixora/snakeyaml/SnakeyamlGuide.java
+++ src/main/java/tech/intellispaces/ixora/snakeyaml/SnakeyamlGuide.java
@@ -6,13 +6,10 @@ import tech.intellispaces.ixora.commons.structures.properties.MapBasedProperties
 import tech.intellispaces.ixora.structures.properties.InvalidPropertyException;
 import tech.intellispaces.ixora.structures.properties.PropertiesHandle;
 import tech.intellispaces.framework.core.annotation.Mapper;
-import tech.intellispaces.ixora.structures.properties.YamlBytesToPropertiesTransition;
 import tech.intellispaces.ixora.structures.properties.YamlStringToPropertiesTransition;
 
-import java.io.ByteArrayInputStream;
-
 @Guide
-public class SnakeyamlGuide implements YamlStringToPropertiesTransition, YamlBytesToPropertiesTransition {
+public class SnakeyamlGuide implements YamlStringToPropertiesTransition {
 
   @Mapper
   @Override
@@ -20,17 +17,6 @@ public class SnakeyamlGuide implements YamlStringToPropertiesTransition, YamlByt
     try {
       var yaml = new Yaml();
       return new MapBasedPropertiesHandleImpl(yaml.load(string));
-    } catch (Exception e) {
-      throw InvalidPropertyException.withCauseAndMessage(e, "Failed to load YAML");
-    }
-  }
-
-  @Mapper
-  @Override
-  public PropertiesHandle yamlBytesToProperties(byte[] bytes) throws InvalidPropertyException {
-    try {
-      var yaml = new Yaml();
-      return new MapBasedPropertiesHandleImpl(yaml.load(new ByteArrayInputStream(bytes)));
     } catch (Exception e) {
       throw InvalidPropertyException.withCauseAndMessage(e, "Failed to load YAML");
     }
--- src/test/java/tech/intellispaces/ixora/snakeyaml/SnakeyamlGuideTest.java
+++ src/test/java/tech/intellispaces/ixora/snakeyaml/SnakeyamlGuideTest.java
@@ -1,16 +1,15 @@
 package tech.intellispaces.ixora.snakeyaml;
 
-import org.junit.jupiter.api.Test;
+import tech.intellispaces.ixora.structures.properties.YamlStringToPropertiesTransition;
 import tech.intellispaces.ixora.structures.properties.YamlStringToPropertiesTransitionTest;
 
 /**
  * Tests for {@link SnakeyamlGuide} class.
  */
-public class SnakeyamlGuideTest {
+public class SnakeyamlGuideTest implements YamlStringToPropertiesTransitionTest {
 
-  @Test
-  public void testYamlStringToProperties() {
-    SnakeyamlGuide guide = new SnakeyamlGuide();
-    YamlStringToPropertiesTransitionTest.allTests(guide);
+  @Override
+  public YamlStringToPropertiesTransition guide() {
+    return new SnakeyamlGuide();
   }
 }
