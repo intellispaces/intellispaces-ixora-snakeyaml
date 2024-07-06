2e7610f1ee4ffd4411176cfbd01513b80c4cd2a1
lavor <lavor.andrey@gmail.com>
2024-07-06 09:18:11 +0600

    Object handles implementation updated

--- src/main/java/tech/intellispaces/ixora/snakeyaml/SnakeYamlStringToPropertiesGuide.java
+++ src/main/java/tech/intellispaces/ixora/snakeyaml/SnakeYamlStringToPropertiesGuide.java
@@ -3,7 +3,7 @@ package tech.intellispaces.ixora.snakeyaml;
 import org.yaml.snakeyaml.Yaml;
 import tech.intellispaces.framework.core.annotation.Guide;
 import tech.intellispaces.framework.core.annotation.Mapper;
-import tech.intellispaces.ixora.commons.structures.properties.MapBasedPropertiesHandleImpl;
+import tech.intellispaces.ixora.commons.structures.properties.MapBasedProperties;
 import tech.intellispaces.ixora.structures.exception.InvalidPropertyException;
 import tech.intellispaces.ixora.structures.properties.PropertiesHandle;
 import tech.intellispaces.ixora.structures.properties.YamlStringToPropertiesGuide;
@@ -16,7 +16,7 @@ public class SnakeYamlStringToPropertiesGuide implements YamlStringToPropertiesG
   public PropertiesHandle yamlStringToProperties(String string) throws InvalidPropertyException {
     try {
       var yaml = new Yaml();
-      return new MapBasedPropertiesHandleImpl(yaml.load(string));
+      return new MapBasedProperties(yaml.load(string));
     } catch (Exception e) {
       throw InvalidPropertyException.withCauseAndMessage(e, "Failed to load YAML");
     }
